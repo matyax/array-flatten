068a7b4098d07ad574547f4e71f1b589368f653c
Matias Chomicki <matyax@gmail.com>
2016-02-12 19:26:26 -0300

    Add array flatten algorithm

--- src/array-flatten.js
+++ src/array-flatten.js
@@ -1,3 +1,19 @@
-function arrayFlatten(input) {
-    return input;
+function arrayFlatten(input) { 'use strict';
+    if ((input.constructor !== Array) || (! input.length)) {
+        return [];
+    }
+
+    var output = [];
+
+    input.forEach(function (element) {
+        if (element.constructor === Array) {
+            output = output.concat(arrayFlatten(element));
+
+            return;
+        }
+
+        output.push(element);
+    });
+
+    return output;
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 (function (global) {
   'use strict';
 
-  var input  = [[1,2,[3]],4],
+  var input  = [[1, 2, [3, [4, [5]]]], 6],
       output = arrayFlatten(input);
 
   console.log(output);
